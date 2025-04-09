79e70429c577553fa4d700cc97502a370b25c1a1
xertrec <alillolindo@gmail.com>
2025-04-09 11:58:12 +0200

    Improvements with weather description

--- grails-app/domain/org/udl/Weather.groovy
+++ grails-app/domain/org/udl/Weather.groovy
@@ -16,4 +16,37 @@ class Weather {
         weatherCode nullable: false
         temperature nullable: false
     }
+
+    String weatherDescription = WEATHER_DESCRIPTIONS.getOrDefault(weatherCode, "Unknown code: ${weatherCode}")
+
+    private static final Map<Integer, String> WEATHER_DESCRIPTIONS = [
+            0 : "Clear sky",
+            1 : "Mainly clear",
+            2 : "Partly cloudy",
+            3 : "Overcast",
+            45: "Fog",
+            48: "Depositing rime fog",
+            51: "Drizzle: Light intensity",
+            53: "Drizzle: Moderate intensity",
+            55: "Drizzle: Dense intensity",
+            56: "Freezing Drizzle: Light intensity",
+            57: "Freezing Drizzle: Dense intensity",
+            61: "Rain: Slight intensity",
+            63: "Rain: Moderate intensity",
+            65: "Rain: Heavy intensity",
+            66: "Freezing Rain: Light intensity",
+            67: "Freezing Rain: Heavy intensity",
+            71: "Snow fall: Slight intensity",
+            73: "Snow fall: Moderate intensity",
+            75: "Snow fall: Heavy intensity",
+            77: "Snow grains",
+            80: "Rain showers: Slight",
+            81: "Rain showers: Moderate",
+            82: "Rain showers: Violent",
+            85: "Snow showers: Slight",
+            86: "Snow showers: Heavy",
+            95: "Thunderstorm: Slight or moderate",
+            96: "Thunderstorm with slight hail",
+            99: "Thunderstorm with heavy hail"
+    ].asImmutable()
 }
